0a9ccca2248d913b2497348cc61311cbd1a6b915
Muhammad Sandi Haikal <muhammadsandi1911@gmail.com>
2022-08-30 10:35:54 +0700

    Membuat Migration Data Siswa

---
+++ app/Database/Migrations/2022-08-30-031543_Siswa.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Siswa extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id_siswa'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => true,
+				'auto_increment' => true,
+			],
+			'nama'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+			],
+            'no_absen' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => '100'
+            ],
+			'jenis_kelamin'       => [
+				'type'              => 'ENUM',
+				'constraint'        => "'pria','wanita'",
+			],
+			'no_telp' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'email' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'alamat' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255',
+			],
+			'created_at' => [
+				'type'           => 'DATETIME',
+				'null'           => true,
+			],
+			'updated_at' => [
+				'type'           => 'DATETIME',
+				'null'           => true,
+			]
+		]);
+		$this->forge->addPrimaryKey('id_siswa');
+		$this->forge->createTable('siswa');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('siswa');
+	}
+}

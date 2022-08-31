c67b72626a40fce78b3fdf8bf99fc12472e6048a
Muhammad Sandi Haikal <muhammadsandi1911@gmail.com>
2022-08-31 11:17:42 +0700

    Menambahkan Template

--- app/Views/guru/index.php
+++ app/Views/guru/index.php
@@ -7,51 +7,54 @@
             <h1>Data Guru</h1>
         </div>
 
-<div class="content">
-    <div class="card">
-        <div class="card-body">
-            <?php if (!empty(session()->getFlashdata('message'))) : ?>
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <?php echo session()->getFlashdata('message'); ?>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            <?php endif; ?>
-            <a href="<?= base_url('/guru/create'); ?>" class="btn btn-primary mb-3">Tambah</a>
-            <table class="table table-bordered">
-                <tr>
-                    <th>No</th>
-                    <th>Nama</th>
-                    <th>Mata Pelajaran</th>
-                    <th>Jenis Kelamin</th>
-                    <th>No Telp</th>
-                    <th>Email</th>
-                    <th>Alamat</th>
-                    <th>Action</th>
-                </tr>
-                <?php
+        <div class="content">
+            <div class="card">
+                <div class="card-body">
+                    <?php if (!empty(session()->getFlashdata('message'))) : ?>
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <?php echo session()->getFlashdata('message'); ?>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <?php endif; ?>
+                    <a href="<?= base_url('/guru/create'); ?>" class="btn btn-primary mb-3">Tambah</a>
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Mata Pelajaran</th>
+                            <th>Jenis Kelamin</th>
+                            <th>No Telp</th>
+                            <th>Email</th>
+                            <th>Alamat</th>
+                            <th>Action</th>
+                        </tr>
+                        <?php
                 $no = 1;
                 foreach ($guru as $row) {
                 ?>
-                    <tr>
-                        <td><?= $no++; ?></td>
-                        <td><?= $row->nama; ?></td>
-                        <td><?= $row->mata_pelajaran; ?></td>
-                        <td><?= $row->jenis_kelamin; ?></td>
-                        <td><?= $row->no_telp; ?></td>
-                        <td><?= $row->email; ?></td>
-                        <td><?= $row->alamat; ?></td>
-                        <td>
-                            <a title="Edit" href="<?= base_url("guru/edit/$row->id_guru"); ?>" class="btn btn-info mt-2">Edit</a>
-                            <a title="Delete" href="<?= base_url("guru/delete/$row->id_guru") ?>" class="btn btn-danger mb-2 mt-2" onclick="return confirm('Apakah Anda yakin ingin menghapus data ?')">Delete</a>
-                        </td>
-                    </tr>
-                <?php
+                        <tr>
+                            <td><?= $no++; ?></td>
+                            <td><?= $row->nama; ?></td>
+                            <td><?= $row->mata_pelajaran; ?></td>
+                            <td><?= $row->jenis_kelamin; ?></td>
+                            <td><?= $row->no_telp; ?></td>
+                            <td><?= $row->email; ?></td>
+                            <td><?= $row->alamat; ?></td>
+                            <td>
+                                <a title="Edit" href="<?= base_url("guru/edit/$row->id_guru"); ?>"
+                                    class="btn btn-info mt-2">Edit</a>
+                                <a title="Delete" href="<?= base_url("guru/delete/$row->id_guru") ?>"
+                                    class="btn btn-danger mb-2 mt-2"
+                                    onclick="return confirm('Apakah Anda yakin ingin menghapus data ?')">Delete</a>
+                            </td>
+                        </tr>
+                        <?php
                 }
                 ?>
-            </table>
+                    </table>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<?= $this->endSection('content'); ?>
+        <?= $this->endSection('content'); ?>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -1,74 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Data Siswa dan Guru</title>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Data Siswa dan Guru</title>
 
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="../../assets/css/style.css">
-  <link rel="stylesheet" href="../../assets/css/components.css">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/components.css">
 </head>
 
 <body>
-  <div id="app">
-    <div class="main-wrapper">
-      <div class="navbar-bg"></div>
-      <nav class="navbar navbar-expand-lg main-navbar">
-      </nav>
-      <div class="main-sidebar">
-        <aside id="sidebar-wrapper">
-          <div class="sidebar-brand">
-            <a href="#">Stisla</a>
-          </div>
-          <div class="sidebar-brand sidebar-brand-sm">
-            <a href="#">St</a>
-          </div>
-          <ul class="sidebar-menu">
-              <li class="menu-header">Dashboard</li>
-              <li><a href="/" class="nav-link"><i class="far fa-fire"></i><span>Dashboard</span></a></li>
-              <li class="menu-header">Starter</li>
-              <li><a class="nav-link" href="/guru"><i class="fa fa-users"></i> <span>Data Guru</span></a></li>
-              <li><a class="nav-link" href="/siswa"><i class="fa fa-users"></i> <span>Data Siswa</span></a></li>
-            </ul>
+    <div id="app">
+        <div class="main-wrapper">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+            </nav>
+            <div class="main-sidebar">
+                <aside id="sidebar-wrapper">
+                    <div class="sidebar-brand">
+                        <a href="#">Stisla</a>
+                    </div>
+                    <div class="sidebar-brand sidebar-brand-sm">
+                        <a href="#">St</a>
+                    </div>
+                    <ul class="sidebar-menu">
+                        <li class="menu-header">Dashboard</li>
+                        <li><a href="/" class="nav-link"><i class="far fa-fire"></i><span>Dashboard</span></a></li>
+                        <li class="menu-header">Starter</li>
+                        <li><a class="nav-link" href="/guru"><i class="fa fa-users"></i> <span>Data Guru</span></a></li>
+                        <li><a class="nav-link" href="/siswa"><i class="fa fa-users"></i> <span>Data Siswa</span></a>
+                        </li>
+                    </ul>
 
-            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-              <a href="https://github.com/muhammad-sandi/Data-Guru-dan-Siswa" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                <i class="fas fa-sign-out-alt"></i> Git Hub
-              </a>
+                    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+                        <a href="https://github.com/muhammad-sandi/Data-Guru-dan-Siswa"
+                            class="btn btn-primary btn-lg btn-block btn-icon-split">
+                            <i class="fas fa-sign-out-alt"></i> Git Hub
+                        </a>
+                    </div>
+                </aside>
             </div>
-        </aside>
-      </div>
 
-      <!-- Main Content -->
-    <main role="main" class="flex-shrink-0">
-        <?= $this->renderSection('content') ?>
-    </main>
+            <!-- Main Content -->
+            <main role="main" class="flex-shrink-0">
+                <?= $this->renderSection('content') ?>
+            </main>
 
-  <!-- General JS Scripts -->
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="../../assets/js/stisla.js"></script>
+            <!-- General JS Scripts -->
+            <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+                integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js">
+            </script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+            <script src="../../assets/js/stisla.js"></script>
 
-  <!-- JS Libraies -->
-  <script src="../../ssets/modules/jquery.sparkline.min.js"></script>
-  <script src="../../assets/modules/chart.min.js"></script>
-  <script src="../../assets/modules/owlcarousel2/dist/owl.carousel.min.js"></script>
-  <script src="../../assets/modules/summernote/summernote-bs4.js"></script>
-  <script src="../../assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+            <!-- JS Libraies -->
+            <script src="../../ssets/modules/jquery.sparkline.min.js"></script>
+            <script src="../../assets/modules/chart.min.js"></script>
+            <script src="../../assets/modules/owlcarousel2/dist/owl.carousel.min.js"></script>
+            <script src="../../assets/modules/summernote/summernote-bs4.js"></script>
+            <script src="../../assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
 
-  <!-- Template JS File -->
-  <script src="../../assets/js/scripts.js"></script>
-  <script src="../../assets/js/custom.js"></script>
+            <!-- Template JS File -->
+            <script src="../../assets/js/scripts.js"></script>
+            <script src="../../assets/js/custom.js"></script>
 
-  <!-- Page Specific JS File -->
-  <script src="../../assets/js/page/index.js"></script>
+            <!-- Page Specific JS File -->
+            <script src="../../assets/js/page/index.js"></script>
 </body>
+
 </html>
